254b858d097c286549b71bf50d2fe552c8271661
miftakhurrokhmah01 <90810479+miftakhurrokhmah01@users.noreply.github.com>
2021-11-24 06:44:27 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+//tambah mift
   @override
   Widget build(BuildContext context) {
     dubugShowCheckedModeBanner:
